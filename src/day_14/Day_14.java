3fa1897fd5ad1cff79899489c26b9e0ccd89d6cd
yanik-recke <yanik.recke@mailfence.com>
2023-12-13 17:32:35 +0100

    Prep for day 14

---
+++ src/day_14/Day_14.java
@@ -0,0 +1,28 @@
+package day_14;
+
+
+/**
+ * Lösung für Tag 14.
+ * 
+ * @author Yanik Recke
+ */
+public class Day_14 {
+
+	
+	public static void main(String[] args) {
+		String pathToInput = "src/day_14/input.txt";
+		
+		System.out.print(part1(pathToInput) + " - ");
+		System.out.println(part2(pathToInput));
+	}
+	
+	
+	private static long part1(String path) {
+		return 0;
+	}
+	
+	private static long part2(String path) {
+		return 0;
+	}
+	
+}
