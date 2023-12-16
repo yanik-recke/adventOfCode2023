3c6df561a94b109893f64bae0c624a1acddefbdd
yanik-recke <yanik.recke@mailfence.com>
2023-12-16 11:44:11 +0100

    Working on day 16 p1

---
+++ src/day_16/Day_16.java
@@ -0,0 +1,30 @@
+package day_16;
+
+
+/**
+ * Lösung für Tag 16.
+ * 
+ * @author Yanik Recke
+ */
+public class Day_16 {
+
+	
+	public static void main(String[] args) {
+		String pathToInput = "src/day_16/input.txt";
+		
+		System.out.print(part1(pathToInput) + " - ");
+		System.out.println(part2(pathToInput));
+	}
+	
+	
+	private static long part1(String path) {
+		char[][] field = helpers.HelperMethods.getInputAsTwoDimensionalCharArray(path);
+		
+		return 0;
+	}
+	
+	private static long part2(String path) {
+		return 0;
+	}
+	
+}
