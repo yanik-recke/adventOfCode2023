81451a47589b0bf54687c2b55a651170a64ab192
yrec <yanik.recke@mailfence.com>
2023-12-18 23:28:28 +0100

    Prep for day 18

---
+++ src/day_18/Day_18.java
@@ -0,0 +1,28 @@
+package day_18;
+
+
+/**
+ * Lösung für Tag 18.
+ * 
+ * @author Yanik Recke
+ */
+public class Day_18 {
+
+	
+	public static void main(String[] args) {
+		String pathToInput = "src/day_18/input.txt";
+		
+		System.out.print(part1(pathToInput) + " - ");
+		System.out.println(part2(pathToInput));
+	}
+	
+	
+	private static long part1(String path) {
+		return 0;
+	}
+	
+	private static long part2(String path) {
+		return 0;
+	}
+	
+}
